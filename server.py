893f24e450388d86cd5289a559e064e0fad4d33d
matias <matias@hillmann.com.ar>
2021-02-28 17:09:53 -0300

    Primer cambio

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 from _thread import *
-import sys
+from player import Player
+import pickle
 
 server = "10.9.20.1"
 port = 5555
@@ -15,26 +16,19 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for a connection, Server Started")
 
-def read_pos(str):
-        str = str.split(",")
-        return int(str[0]), int(str[1])
 
-def make_pos(tup):
-        return str(tup[0]) + "," + str(tup[1])
-
-
-pos = [(0,0), (100,100)]
+players = [Player(0,0,50,50,(255,0,0)), Player(100,100,50,50,(0,0,255))]
 
 def threaded_client(conn, player):
 	#conn.send(str.encode("Connected"))
-	conn.send(str.encode(make_pos(pos[player])))
+	conn.send(pickle.dumps(players[player]))
 	reply = ""
 	while True:
 		try:
 			#data = conn.recv(2048)
-			data = read_pos(conn.recv(2048).decode())
+			data = pickle.loads(conn.recv(2048))
 			#reply = data.decode("utf-8")
-			pos[player] = data
+			players[player] = data
 
 
 			if not data:	#safenet para que no pasen loops infinitos
@@ -42,14 +36,14 @@ def threaded_client(conn, player):
 				break
 			else:
 				if player == 1:
-					reply = pos[0]
+					reply = players[0]
 				else:
-					reply = pos[1]
+					reply = players[1]
 
 				print("Recieved: ", data)
 				print("Sending: ", reply)
 
-			conn.sendall(str.encode(make_pos(reply)))
+			conn.sendall(pickle.dumps(reply))
 		except:
 			break
 	print("Lost connection")
